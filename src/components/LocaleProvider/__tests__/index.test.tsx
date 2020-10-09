c265532f7a8b30ffa354c8897049d6ddb945093a
akai <akainice@gmail.com>
2020-10-09 17:07:47 +0800

    test: update LocaleProvider

--- src/components/LocaleProvider/__tests__/index.test.tsx
+++ src/components/LocaleProvider/__tests__/index.test.tsx
@@ -1,24 +1,103 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import { LocaleProvider } from '..';
-import locales from '../locales';
-import { Time } from '../../Time/';
+import { LocaleProvider, LocaleContext, useLocale } from '..';
 
 afterEach(cleanup);
 
-const App = () => (
-  <div>
-    <Time date={1577851200000} />
-  </div>
-);
-
 describe('<LocaleProvider />', () => {
-  Object.keys(locales).forEach((locale) => {
-    it(`should display the text as ${locale}`, () => {
-      const { container } = render(<LocaleProvider locales={locales} locale={locale}>
-          <App />
-        </LocaleProvider>);
-      expect(container).toMatchSnapshot();
-    });
-  })
+  it(`should provide the locale`, () => {
+    function Test() {
+      const { locale } = React.useContext(LocaleContext);
+      return <span data-testid="span">{locale}</span>;
+    }
+
+    const { getByTestId } = render(
+      <LocaleProvider locale="en-US">
+        <Test />
+      </LocaleProvider>,
+    );
+
+    expect(getByTestId('span')).toHaveTextContent('en-US');
+  });
+
+  it(`should provide the locale by useLocale`, () => {
+    function Test() {
+      const { trans } = useLocale();
+      return <span data-testid="span">{trans('SendConfirm').send}</span>;
+    }
+
+    const { getByTestId } = render(
+      <LocaleProvider locale="en-US">
+        <Test />
+      </LocaleProvider>,
+    );
+
+    expect(getByTestId('span')).toHaveTextContent('Send');
+  });
+
+  it(`should provide the locale by useLocale with component name`, () => {
+    function Test() {
+      const { trans } = useLocale('SendConfirm');
+      return (
+        <div>
+          <span data-testid="span1">{trans().title}</span>
+          <span data-testid="span2">{trans('send')}</span>
+        </div>
+      );
+    }
+
+    const { getByTestId } = render(
+      <LocaleProvider locale="en-US">
+        <Test />
+      </LocaleProvider>,
+    );
+
+    expect(getByTestId('span1')).toHaveTextContent('Send photo');
+    expect(getByTestId('span2')).toHaveTextContent('Send');
+  });
+
+  it(`should provide the custom locale by useLocale`, () => {
+    function Test() {
+      const { trans } = useLocale('Test');
+      return <span data-testid="span">{trans()}</span>;
+    }
+
+    const { getByTestId } = render(
+      <LocaleProvider locales={{ Test: 'test' }}>
+        <Test />
+      </LocaleProvider>,
+    );
+
+    expect(getByTestId('span')).toHaveTextContent('test');
+  });
+
+  it(`should provide the valid locale`, () => {
+    function Test() {
+      const { trans } = useLocale();
+      return <span data-testid="span">{trans('SendConfirm').send}</span>;
+    }
+
+    const { getByTestId } = render(
+      <LocaleProvider locale="test">
+        <Test />
+      </LocaleProvider>,
+    );
+
+    expect(getByTestId('span')).toHaveTextContent('Send');
+  });
+
+  it(`should get the locale by useLocale`, () => {
+    function Test() {
+      const { locale } = useLocale();
+      return <span data-testid="span">{locale}</span>;
+    }
+
+    const { getByTestId } = render(
+      <LocaleProvider locale="en-US">
+        <Test />
+      </LocaleProvider>,
+    );
+
+    expect(getByTestId('span')).toHaveTextContent('en-US');
+  });
 });
