76add417e6c8a9317c5d303165440988638cef92
akai <akainice@gmail.com>
2021-01-03 21:39:04 +0800

    feat: [Composer] add `data-action-icon` to `.Composer-actions`

--- src/components/Composer/Action.tsx
+++ src/components/Composer/Action.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IconButton, IconButtonProps } from '../IconButton';
 
 export const Action: React.FC<IconButtonProps> = (props) => (
-  <div className="Composer-actions">
+  <div className="Composer-actions" data-action-icon={props.icon}>
     <IconButton {...props} />
   </div>
 );
