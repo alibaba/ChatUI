7faf0fdb14d7d35a14e2854304bd710bf04a7e03
akai <akainice@gmail.com>
2021-03-01 20:52:29 +0800

    test: update List test cases

--- src/components/List/__tests__/index.test.tsx
+++ src/components/List/__tests__/index.test.tsx
@@ -6,27 +6,32 @@ afterEach(cleanup);
 
 describe('<List />', () => {
   it('should support bordered', () => {
-    const { getByRole } = render(<List bordered={true}>
-      <ListItem content="Hello ChatUI" />
-    </List>);
+    const { getByRole } = render(
+      <List bordered={true}>
+        <ListItem content="Hello ChatUI" />
+      </List>,
+    );
     const list = getByRole('list');
-    expect(list).toHaveClass('bordered');
+    expect(list).toHaveClass('List--bordered');
   });
 
   it('should support right icon', () => {
-    const { getByRole } = render(<List>
-      <ListItem content="Hello ChatUI" rightIcon="chevron-right" />
-    </List>);
+    const { getByRole } = render(
+      <List>
+        <ListItem content="Hello ChatUI" rightIcon="chevron-right" />
+      </List>,
+    );
     const listItem = getByRole('listitem');
     expect(listItem.querySelectorAll('.Icon').length).toBe(1);
   });
 
   it('should support as', () => {
-    const { getByRole } = render(<List>
-      <ListItem as="a" content="Hello ChatUI" rightIcon="chevron-right" />
-    </List>);
+    const { getByRole } = render(
+      <List>
+        <ListItem as="a" content="Hello ChatUI" rightIcon="chevron-right" />
+      </List>,
+    );
     const list = getByRole('list');
     expect(list.querySelectorAll('a').length).toBe(1);
   });
-
 });
