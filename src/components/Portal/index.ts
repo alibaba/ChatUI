a372694f33becd776e80760c6a1f314cc276b799
akai <akainice@gmail.com>
2021-05-12 21:14:48 +0800

    feat: [Portal] `container` support ref object

--- src/components/Portal/index.ts
+++ src/components/Portal/index.ts
@@ -1,17 +1,27 @@
-import { useLayoutEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-export type PortalProps = {
-  container?: {};
-  onRendered: () => void;
-};
-
-function getContainer(el: any) {
-  return typeof el === 'function' ? el() : el;
+export interface PortalProps {
+  container?: React.RefObject<any> | Element | (() => Element) | null;
+  onRendered?: () => void;
 }
+
+function getEl(el: any) {
+  if (!el) return null;
+
+  if (el instanceof Element) {
+    return el;
+  }
+  return typeof el === 'function' ? el() : el.current || el;
+}
+
 export const Portal: React.FC<PortalProps> = (props) => {
   const { children, container = document.body, onRendered } = props;
-  const mountNode = getContainer(container);
+  const [mountNode, setMountNode] = useState<Element | null>(null);
+
+  useEffect(() => {
+    setMountNode(getEl(container));
+  }, [container]);
 
   useLayoutEffect(() => {
     if (onRendered && mountNode) {
