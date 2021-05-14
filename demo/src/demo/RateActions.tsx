fb4d2aa8927d867c52170d3c84289154c68e0130
akai <akainice@gmail.com>
2021-05-14 10:45:41 +0800

    fix: [RateActions] set default title
    BREAKING CHANGE: change `good/bad` to `up/down`

--- demo/src/demo/RateActions.tsx
+++ demo/src/demo/RateActions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DemoPage, DemoSection } from '../components';
-import { RateActions } from '../../../src';
+import { RateActions, LocaleProvider } from '../../../src';
 import '../../../src/styles/index.less';
 
 export default () => (
@@ -11,6 +11,26 @@ export default () => (
           console.log(val);
         }}
       />
+    </DemoSection>
+    <DemoSection title="多语言（英文）">
+      <LocaleProvider locale="en-US">
+        <RateActions
+          onClick={(val: string) => {
+            console.log(val);
+          }}
+        />
+      </LocaleProvider>
+    </DemoSection>
+    <DemoSection title="修改 title">
+      <LocaleProvider locale="en-US">
+        <RateActions
+          upTitle="Like"
+          downTitle="Unlike"
+          onClick={(val: string) => {
+            console.log(val);
+          }}
+        />
+      </LocaleProvider>
     </DemoSection>
   </DemoPage>
 );
