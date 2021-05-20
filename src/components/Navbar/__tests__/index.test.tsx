eda8b51f3b165d6154952b923d27c0e81f864d13
akai <akainice@gmail.com>
2021-05-20 15:53:25 +0800

    test: [Navbar] fix rightContent case

--- src/components/Navbar/__tests__/index.test.tsx
+++ src/components/Navbar/__tests__/index.test.tsx
@@ -11,28 +11,23 @@ describe('<Navbar />', () => {
       icon: 'chevron-left',
       onClick: jest.fn(),
     };
-    const { container } = render(<Navbar
-      title={title}
-      leftContent={leftContent}
-    />);
+    const { container } = render(<Navbar title={title} leftContent={leftContent} />);
     expect(container.querySelectorAll('.Navbar-left .IconBtn').length).toBe(1);
   });
 
   it('should support rightContent', () => {
     const title = 'Hello ChatUI';
-    const rightContent = [{
-      icon: 'apps',
-      type: 'apps',
-      onClick: jest.fn(),
-    }, {
-      icon: 'cart',
-      type: 'cart',
-      onClick: jest.fn(),
-    }];
-    const { container } = render(<Navbar
-      title={title}
-      rightContent={rightContent}
-    />);
+    const rightContent = [
+      {
+        icon: 'apps',
+        onClick: jest.fn(),
+      },
+      {
+        icon: 'cart',
+        onClick: jest.fn(),
+      },
+    ];
+    const { container } = render(<Navbar title={title} rightContent={rightContent} />);
     expect(container.querySelectorAll('.Navbar-right .IconBtn').length).toBe(2);
   });
 });
