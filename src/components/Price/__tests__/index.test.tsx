c296c7bacfff99baf5d4624c30ed9f318e0bffbf
akai <akainice@gmail.com>
2021-05-25 22:34:10 +0800

    test: [Price] fix price type

--- src/components/Price/__tests__/index.test.tsx
+++ src/components/Price/__tests__/index.test.tsx
@@ -6,29 +6,29 @@ afterEach(cleanup);
 
 describe('<Price />', () => {
   it('should render the price', () => {
-    const { getByTestId } = render(<Price price="12" data-testid="price" />);
+    const { getByTestId } = render(<Price price={12} data-testid="price" />);
     const price = getByTestId('price');
 
     expect(price.querySelector('.Price-integer')).toHaveTextContent('12');
   });
 
   it('should render the price', () => {
-    const { getByTestId } = render(<Price price="12.34" data-testid="price" />);
+    const { getByTestId } = render(<Price price={12.34} data-testid="price" />);
     const price = getByTestId('price');
 
     expect(price.querySelector('.Price-integer')).toHaveTextContent('12');
-    expect(price.querySelector('.Price-decimal')).toHaveTextContent('34');
+    expect(price.querySelector('.Price-fraction')).toHaveTextContent('34');
   });
 
   it('should render the currency', () => {
-    const { getByTestId } = render(<Price price="12" currency="¥" data-testid="price" />);
+    const { getByTestId } = render(<Price price={12} currency="¥" data-testid="price" />);
     const price = getByTestId('price');
 
     expect(price.querySelector('.Price-currency')).toHaveTextContent('¥');
   });
 
   it('should have a original class', () => {
-    const { getByTestId } = render(<Price price="12" original data-testid="price" />);
+    const { getByTestId } = render(<Price price={12} original data-testid="price" />);
     const price = getByTestId('price');
 
     expect(price).toHaveClass('Price--original');
