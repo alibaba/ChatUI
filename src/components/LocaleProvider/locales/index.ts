48942b5ef17083ddb7a817211efe0685f6e55351
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2021-06-27 15:58:12 +0300

    Added tr_TR locale

--- src/components/LocaleProvider/locales/index.ts
+++ src/components/LocaleProvider/locales/index.ts
@@ -2,10 +2,12 @@ import arEG from './ar_EG';
 import enUS from './en_US';
 import frFR from './fr_FR';
 import zhCN from './zh_CN';
+import trTR from './tr_TR';
 
 export default {
   'ar-EG': arEG, // 阿拉伯
   'fr-FR': frFR, // 法语
   'en-US': enUS, // 英语（美式）
   'zh-CN': zhCN, // 简体中文
+  'tr-TR': trTR, // Turkish
 };
