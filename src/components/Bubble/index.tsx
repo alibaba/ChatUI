2ada2a6b4f52540233e8b9f0eb9321ca23da8429
akai <akainice@gmail.com>
2022-01-11 11:12:30 +0800

    fix: [Bubble] rest props

--- src/components/Bubble/index.tsx
+++ src/components/Bubble/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export interface BubbleProps {
+export interface BubbleProps extends React.HTMLAttributes<HTMLDivElement> {
   type?: string;
   content?: React.ReactNode;
 }
 
 export const Bubble: React.FC<BubbleProps> = (props) => {
-  const { type = 'text', content, children } = props;
+  const { type = 'text', content, children, ...other } = props;
   return (
-    <div className={`Bubble ${type}`} data-type={type}>
+    <div className={`Bubble ${type}`} data-type={type} {...other}>
       {content && <p>{content}</p>}
       {children}
     </div>
