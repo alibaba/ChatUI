f9d1d6debf81a6f24ea46a7365b3695a0592c84f
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-06-15 21:29:55 -0700

    Add Japanese localization

--- src/components/LocaleProvider/locales/index.ts
+++ src/components/LocaleProvider/locales/index.ts
@@ -2,10 +2,12 @@ import arEG from './ar_EG';
 import enUS from './en_US';
 import frFR from './fr_FR';
 import zhCN from './zh_CN';
+import jaJP from './ja_JP';
 
 export default {
   'ar-EG': arEG, // 阿拉伯
   'fr-FR': frFR, // 法语
   'en-US': enUS, // 英语（美式）
   'zh-CN': zhCN, // 简体中文
+  'ja-JP': jaJP, // 日语
 };
---
+++ src/components/LocaleProvider/locales/ja_JP.ts
@@ -0,0 +1,37 @@
+export default {
+  BackBottom: {
+    newMsgOne: '{n}件の新着',
+    newMsgOther: '{n}件の新着',
+    bottom: '最新',
+  },
+  Time: {
+    weekdays: '日_月_火_水_木_金_土'.split('_'),
+    formats: {
+      LT: 'HH:mm', // 00:32
+      lll: 'YYYY年M月D日 HH:mm', // 2019年2月22日 00:28
+      WT: 'dddd, HH:mm', // 月, 00:32
+      YT: '昨日 HH:mm', // 昨日 00:32
+    },
+  },
+  Composer: {
+    send: '送信',
+  },
+  SendConfirm: {
+    title: '写真を送信',
+    send: '送信',
+    cancel: 'キャンセル',
+  },
+  RateActions: {
+    up: '賛成',
+    down: '反対',
+  },
+  Recorder: {
+    hold2talk: '押しながら録音',
+    release2send: '離して送信',
+    releaseOrSwipe: '離して送信、スワイプでキャンセル',
+    release2cancel: '離してキャンセル',
+  },
+  Search: {
+    search: '検索',
+  },
+};
